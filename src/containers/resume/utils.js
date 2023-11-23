b0df9af44e29ee5ac8fc3403e3eb3a247676b602
Dennis5050 <deniskiplagat204@gmail.com>
2023-11-23 13:18:51 +0300

    updated

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,42 @@
+export const data = {
+    experience: [
+      {
+        title: "Consultant",
+        subTitle: "Raesa, Rs",
+        description:
+          " User Experience, Visual Design",
+      },
+      {
+        title: "Full Stack Developer",
+        subTitle: "Fiver",
+        description:
+          "Web development, Databases, Backend developement",
+      },
+      {
+        title: "Project Manager",
+        subTitle: "Kirinyaga, KENYA",
+        description:
+          "Creative Direction Project Management, Team Leading",
+      },
+    ],
+    education: [
+      {
+        title: "Secondary",
+        subTitle: "Munyuini School",
+        description:
+          "10th Grade",
+      },
+      {
+        title: "Higher Secondary",
+        subTitle: "Soweto School of higher education",
+        description:
+          "12th Grade in Science",
+      },
+      {
+        title: "B.SWE",
+        subTitle: "Kirinyaga university",
+        description:
+          "Bachelor of science in software engineering",
+      },
+    ],
+  };
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,53 @@
+export const skillsData = [
+    {
+      label: "FRONT END",
+      data: [
+        {
+          skillName: "HTML",
+          percentage: "90",
+        },
+        {
+          skillName: "CSS",
+          percentage: "80",
+        },
+        {
+          skillName: "JAVASCRIPT",
+          percentage: "70",
+        },
+      ],
+    },
+    {
+      label: "BACK END",
+      data: [
+        {
+          skillName: "NODE JS",
+          percentage: "80",
+        },
+        {
+          skillName: "PYTHON",
+          percentage: "55",
+        },
+        {
+          skillName: "PHP",
+          percentage: "35",
+        },
+      ],
+    },
+    {
+      label: "DATABASES",
+      data: [
+        {
+          skillName: "SQL",
+          percentage: "40",
+        },
+        {
+          skillName: "MONGO DB",
+          percentage: "80",
+        },
+        {
+          skillName: "FIREBASE",
+          percentage: "40",
+        },
+      ],
+    },
+  ];
